a3992fb140de5bb35b1e72bb545bb96f0d043cb9
Mark May <mamark90@gmail.com>
2019-06-28 20:23:55 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,15 @@
+require 'pry'
 
+class Course
+  attr_accessor :title, :schedule, :descrription
 
+  @@all = []
+
+  def initialize(title = nil, schedule = nil, descrription = nil)
+    @title = title
+    @schedule = schedule
+    @description = description
+    @@all << self
+  end
+
+end
