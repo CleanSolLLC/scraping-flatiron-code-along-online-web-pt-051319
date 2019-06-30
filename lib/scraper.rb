bfb14f199f6e2e287cac655482ea6b5b30d77ce7
Mark May <mamark90@gmail.com>
2019-06-30 10:37:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,8 +10,8 @@ class Scraper
   def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
       doc.each do |data|
-      binding.pry
       end
+      binding.pry
   end
         #data.doc.css(".post")
       #doc.css(".post").first.css("h2").text
